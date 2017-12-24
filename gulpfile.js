c073280f96005b760646a9c968ecc2589c5edd61
Pedro Serapio <pedro.serapio@gmail.com>
2017-12-24 13:44:09 +0800

    Optimization and SEO added

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,45 @@
 const gulp       = require('gulp'),
       webpack    = require('webpack-stream'),
       sass       = require('gulp-sass'),
-      rename     = require('gulp-rename'),
       minifyCSS  = require('gulp-clean-css'),
       prefixer   = require('gulp-autoprefixer'),
-      eyeglass   = require('eyeglass'),
+      connect    = require('gulp-connect'),
+      rename     = require('gulp-rename'),
       del        = require('del');
 
 const base_path  = './',
       src        = base_path + 'dev/src',
-      dist       = base_path + 'dist/assets';
+      dist       = base_path + 'docs';
 
-const sassOptions = {
-      // put node-sass options you need here.
-      eyeglass: {
-        // put eyeglass options you need here.
-      }
-};
+gulp.task('connect', function(){
+  connect.server({
+    root: dist,
+    livereload: true
+  });
+});
 
 gulp.task('html', function(){
   return gulp.src(src + '/*.html')
-    .pipe(gulp.dest(base_path + 'dist/'));
+    .pipe(gulp.dest( dist + '/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', function(){
+  return gulp.src( src + '/images/**/*' )
+    .pipe(gulp.dest( dist + '/assets/images/' ))
+    .pipe(connect.reload());
 });
 
 gulp.task('threed-models', function(){
   return gulp.src(src + '/threedmodels/**/*')
-    .pipe(gulp.dest(dist + '/threedmodels/'))
+    .pipe(gulp.dest(dist + '/assets/threedmodels/'))
 });
 
 gulp.task('sass', function(){
   return gulp.src(src + '/stylesheet/*.scss')
-    .pipe(sass(eyeglass(sassOptions)).on("error", sass.logError))
+    .pipe(sass({
+      includePaths: ['node_modules/bootstrap-sass/assets/stylesheets/bootstrap']
+    }))
     .pipe(prefixer(
           'Android 2.3',
           'Android >= 4',
@@ -47,20 +56,36 @@ gulp.task('sass', function(){
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(dist + '/css/'));
+    .pipe(gulp.dest(dist + '/assets/css/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('app-scripts', function(){
   return gulp.src( src + '/javascript/app.js' )
     .pipe(webpack( require('./webpack.config.js' )))
     .pipe(rename('app.min.js'))
-    .pipe(gulp.dest(dist + '/js/'));
+    .pipe(gulp.dest(dist + '/assets/js/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('clean', function(){
-  return del('dist/*');
+  return del('docs/*');
+});
+
+gulp.task('watch', function(){
+  gulp.watch( src + '/*.html', ['html'] );
+  gulp.watch( src + '/stylesheet/*.scss', ['sass'] );
+  gulp.watch( src + '/javascript/app.js', ['app-scripts'] );
 });
 
 gulp.task('default', ['clean'], function(){
-  gulp.start('html', 'threed-models', 'sass', 'app-scripts');
+  gulp.start(
+    'html',
+    'images',
+    'threed-models',
+    'sass',
+    'app-scripts',
+    'connect',
+    'watch'
+  );
 });
